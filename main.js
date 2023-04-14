4bd2d6d7e66ee16074e689548a7b36fc4b1afe4a
nea04184 <bmp.tom@yahoo.com>
2023-04-14 16:12:06 +0900

    도서관 동기와 비동기

--- main.js
+++ main.js
@@ -1,39 +1,75 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
 console.log("Hello 혤로world!");
+
+//template는 재사용할 수 있는 껍데기 같은..?
+function templateHTML(title, list, body) {
+  return `<!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateList(filelist) {
+  var list = "<ul>";
+  var i = 0;
+  while (i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list + "</ul>";
+  return list;
+}
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(request.url, true).query;
   var title = queryData.id;
-
-  
-
-
-
-  
-  fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-    var template = `<!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>
-    `;
-    response.writeHead(200);
-    response.end(template);
-  });
+  var pathname = url.parse(request.url, true).pathname;
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      fs.readdir("./data", function (error, filelist) {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var template = templateHTML(
+          title,
+          templateList(filelist),
+          `<h2>${title}</h2>${description}`
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readdir("./data", function (error, filelist) {
+        fs.readFile(
+          `data/${queryData.id}`,
+          "utf8",
+          function (err, description) {
+            var template = templateHTML(
+              title,
+              templateList(filelist),
+              `<h2>${title}</h2>${description}`
+            );
+            response.writeHead(200);
+            response.end(template);
+          }
+        );
+      });
+    }
+  } else {
+    response.writeHead(404);
+    response.end("Not found");
+  }
 });
+
 app.listen(3000);
